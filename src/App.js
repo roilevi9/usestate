21a0293d9fd87e8c859f558092381c4d3df069d8
Roi Levi <roilevi9@gmail.com>
2022-11-27 09:18:19 +0200

    HW 4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import './App.css';
 import Counter from './Counter';
+import { useState } from "react";
+
+
 
 function App() {
+  const [delta, setDelta] = useState(1)
+  const [maxNum, setMaxNum] = useState(10)
+
+  function handleDelta(e){
+  console.log(e);
+  setDelta(Number(e.target.value))
+}
+  function handleMaxNum(m){
+    setMaxNum(Number(m.target.value))
+  }
+
   return (
     <div className="App">
     <h1>Usestate</h1>
-    <Counter />
+    <h3>The Max Num:</h3>
+    <input type= "number" value = {maxNum} onChange = {handleMaxNum}/>
+
+    <h3>Delta:</h3>
+    <input type= "number" value = {delta} onChange = {handleDelta}/>
+    <Counter delta = {delta} maxNum = {maxNum}/>
+    <Counter delta = {delta} maxNum = {maxNum}/>
     </div>
   );
 }
--- src/Counter.js
+++ src/Counter.js
@@ -1,31 +1,33 @@
 import { useState } from "react";
-import './Counter.css';
+//import './Counter.css';
 
-function Counter() {
+function Counter(props) {
     const [count, setCount] = useState(1)
-    const [delta, setDelta] = useState(1)
+    const {delta} = props
+    const {maxNum} = props
 
     function incr(){
+        
         setCount(
             function(oldCount){
-                return oldCount + delta
+                if ((oldCount + delta) > maxNum){
+                    return 1;
+                }
+                
+                else{ return oldCount + delta
+                }
             }
         )
-        console.log(count)
     }
 
     function reset(){
         setCount(1)
     }
 
-    function handleDelta(e){
-        console.log(e);
-        setDelta(Number(e.target.value))
-    }
+
     return (
       <div className="App">
      <h2>Counter</h2>
-     <input type= "number" value = {delta} onChange = {handleDelta}/>
      <p>Counter is at {count}</p>
      <button onClick={incr}>Click to add {delta} to Counter</button>
      <button onClick={reset}>Click to reset the Counter</button>
